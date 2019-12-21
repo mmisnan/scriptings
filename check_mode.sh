5dbdbf8c306a363fde509a2b5d8d8494bcdecf3e
mmisnan <mmisnan@gmail.com>
2019-12-21 19:37:34 +0800

    Update check_mode.sh

--- check_mode.sh
+++ check_mode.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 MMDVMHOST=/etc/mmdvmhost
 STATUS_FILE=/var/tmp/mmdvmstatus
+LOG=/var/log/mmdvmlog
 
 # GET DMR IP address from the current configuration
 IPAddress=$(awk -v TARGET="DMR Network" -F ' *= *' '{ if ($0 ~ /^\[.*\]$/) { gsub(/^\[|\]$/, "", $0); SECTION=$0 } else if (($1 ~ /Address/) && (SECTION==TARGET)) { print $2 }}' $MMDVMHOST)
@@ -13,12 +14,12 @@ fi
 # Check network condition by pinging the DMR master server
 ping -c 1 $IPAddress &> /dev/null
 if [ "$?" = 0 ]; then
-  echo "DMR Network $IPAddress is UP"
 # Check for current status, if it is in standalone or empty
 # Bring it up, else do nothing
   case $Status in
         STANDALONE)
 # We're currently running standalone, change DMR Network to enable
+        echo $(date -u) "DMR Network $IPAddress is UP - NET MODE" >> $LOG
         sed -i '/^\[DMR Network\]$/,/^\[/ s/^Enable=0/Enable=1/' $MMDVMHOST
         systemctl restart mmdvmhost
         echo "NET" > $STATUS_FILE
@@ -27,12 +28,14 @@ if [ "$?" = 0 ]; then
         *)
         systemctl status mmdvmhost &> /dev/null
         if [ "$?" -ne 0 ]; then
+          echo $(date -u) "DMR Network $IPAddress is UP - NET RESTART" >> $LOG
           systemctl restart mmdvmhost
         fi
         echo "NET" > $STATUS_FILE
   esac
 # Internet is down, go into standalone mode regardless
 else
+  echo $(date -u) "DMR Network $IPAddress is DOWN - STANDALONE MODE" >> $LOG
   sed -i '/^\[DMR Network\]$/,/^\[/ s/^Enable=1/Enable=0/' $MMDVMHOST
   systemctl restart mmdvmhost
   echo "STANDALONE" > $STATUS_FILE
