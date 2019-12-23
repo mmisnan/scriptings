97d30199e6e810a37f60ad8d74d00bc93d774724
mmisnan <mmisnan@gmail.com>
2019-12-24 07:19:20 +0800

    Update check_mode.sh

--- check_mode.sh
+++ check_mode.sh
@@ -19,11 +19,13 @@ if [ "$?" = 0 ]; then
   case $Status in
         STANDALONE)
 # We're currently running standalone, change DMR Network to enable
-        sudo mount -o remount,rw
+        sudo mount -o remount,rw &> /dev/null
         echo $(date -u) "DMR Network $IPAddress is UP - NET MODE" >> $LOG
         sed -i '/^\[DMR Network\]$/,/^\[/ s/^Enable=0/Enable=1/' $MMDVMHOST
         systemctl restart mmdvmhost
         echo "NET" > $STATUS_FILE
+# Make sure turn into ro filesystem
+        sudo mount -o remount,ro / &> /dev/null       
         ;;
 # Check if mmdvmhost is running, start if not
         *)
@@ -36,11 +38,11 @@ if [ "$?" = 0 ]; then
   esac
 # Internet is down, go into standalone mode regardless
 else
-  sudo mount -o remount,rw
+  sudo mount -o remount,rw &> /dev/null
   echo $(date -u) "DMR Network $IPAddress is DOWN - STANDALONE MODE" >> $LOG
   sed -i '/^\[DMR Network\]$/,/^\[/ s/^Enable=1/Enable=0/' $MMDVMHOST
   systemctl restart mmdvmhost
   echo "STANDALONE" > $STATUS_FILE
-fi
 # Make sure turn into ro filesystem
-sudo mount -o remount,ro / &> /dev/null
+  sudo mount -o remount,ro / &> /dev/null
+fi
