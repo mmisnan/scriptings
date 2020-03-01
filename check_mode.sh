21de50162708a0b53abf0fbc16183db4b0203c9d
mmisnan <mmisnan@gmail.com>
2020-03-01 17:30:40 +0800

    Update check_mode.sh

--- check_mode.sh
+++ check_mode.sh
@@ -3,8 +3,6 @@ MMDVMHOST=/etc/mmdvmhost
 STATUS_FILE=/var/tmp/mmdvmstatus
 LOG=/var/log/mmdvmlog
 
-# Make it read/write
-mount -o remount,rw &> /dev/null
 # GET DMR IP address from the current configuration
 IPAddress=$(awk -v TARGET="DMR Network" -F ' *= *' '{ if ($0 ~ /^\[.*\]$/) { gsub(/^\[|\]$/, "", $0); SECTION=$0 } else if (($1 ~ /Address/) && (SECTION==TARGET)) { print $2 }}' $MMDVMHOST)
 # Get current status
@@ -17,32 +15,36 @@ ping -c 1 $IPAddress &> /dev/null
 if [ "$?" = 0 ]; then
 # Check if mmdvmhost is running, start if not
   if [ "$Status" == "NET" ]; then
-  systemctl status mmdvmhost &> /dev/null
+    systemctl status mmdvmhost &> /dev/null
     if [ "$?" -ne 0 ]; then
+      mount -o remount,rw &> /dev/null
       echo $(date) "DMR Network $IPAddress is UP - NET RESTART" >> $LOG
       sed -i '/^\[DMR Network\]$/,/^\[/ s/^Enable=0/Enable=1/' $MMDVMHOST
       systemctl restart mmdvmhost
       echo "NET" > $STATUS_FILE
+      mount -o remount,ro / &> /dev/null
     else
       echo $(date) "DMR Network $IPAddress is UP - STILL IN NET MODE" >> $LOG
     fi
 # We're currently running standalone or startup, change DMR Network to enable
   else
+    mount -o remount,rw &> /dev/null
     echo $(date) "DMR Network $IPAddress is UP - GOING NET MODE" >> $LOG
     sed -i '/^\[DMR Network\]$/,/^\[/ s/^Enable=0/Enable=1/' $MMDVMHOST
     systemctl restart mmdvmhost
     echo "NET" > $STATUS_FILE
+    mount -o remount,ro / &> /dev/null
   fi
 # Network is down, go into standalone mode
 else
   if [ "$Status" != "STANDALONE" ]; then
+       mount -o remount,rw &> /dev/null
        echo $(date) "DMR Network $IPAddress is DOWN - GOING STANDALONE MODE" >> $LOG
        sed -i '/^\[DMR Network\]$/,/^\[/ s/^Enable=1/Enable=0/' $MMDVMHOST
        systemctl restart mmdvmhost
        echo "STANDALONE" > $STATUS_FILE
+       mount -o remount,ro / &> /dev/null
   else
     echo $(date) "DMR Network $IPAddress is DOWN - STILL IN STANDALONE MODE" >> $LOG
   fi
 fi
-# Make sure turn into ro filesystem
-mount -o remount,ro / &> /dev/null
